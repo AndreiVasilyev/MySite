6ee850412cd565841f83edaa18208f0d6623baef
Andrei Vasilyev <andrei.vasilyev80@gmail.com>
2020-04-09 22:46:59 +0300

    final release v1.0

--- src/main/java/by/bobruisk/homework/MainApplication.java
+++ src/main/java/by/bobruisk/homework/MainApplication.java
@@ -4,13 +4,14 @@ package by.bobruisk.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EntityScan(
         basePackageClasses = {MainApplication.class, Jsr310JpaConverters.class}
 )
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer{
 
 	
 	public static void main(String[] args) {
